8bc86a37e353e683b1c4bc03ab8bb3a88990f8a9
Hamed Ghazali <hamedghazali53@gmail.com>
2024-06-03 09:28:12 +0330

    boiler plate for http server complete

--- main.go
+++ main.go
@@ -29,6 +29,12 @@ func main() {
 		MaxAge:           300,
 	}))
 
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz", handlerRediness)
+	v1Router.Get("/err", handleErr)
+
+	router.Mount("/v1", v1Router)
+
 	srv := &http.Server{
 		Handler: router,
 		Addr:    ":" + portString,
